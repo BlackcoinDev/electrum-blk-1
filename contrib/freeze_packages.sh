c856123866753ecdf9c276f0e29d58cbbe0037db
BlackcoinDev <dev@blackcoin.dev>
2026-05-27 10:43:54 +0200

    trivial: some small changes from upstream

--- contrib/freeze_packages.sh
+++ contrib/freeze_packages.sh
@@ -16,6 +16,8 @@ if [[ ! "$SYSTEM_PYTHON" ]] ; then
     echo "Please specify which python to use in \$SYSTEM_PYTHON" && exit 1
 fi
 
+"${SYSTEM_PYTHON}" -m hashin -h > /dev/null 2>&1 || { "${SYSTEM_PYTHON}" -m pip install hashin; }
+
 for suffix in '' '-hw' '-binaries' '-binaries-mac' '-build-wine' '-build-mac' '-build-base' '-build-appimage' '-build-android'; do
     reqfile="requirements${suffix}.txt"
 
@@ -23,8 +25,6 @@ for suffix in '' '-hw' '-binaries' '-binaries-mac' '-build-wine' '-build-mac' '-
     "${SYSTEM_PYTHON}" -m venv "$venv_dir"
 
     source "$venv_dir/bin/activate"
-
-    python -m pip install hashin requests > /dev/null 2>&1 || true
 
     echo "Installing dependencies... (${reqfile})"
 
@@ -40,7 +40,7 @@ for suffix in '' '-hw' '-binaries' '-binaries-mac' '-build-wine' '-build-mac' '-
 
     requirements=$(pip freeze --all)
 
-    restricted=$(echo $requirements | python "$contrib/deterministic-build/find_restricted_dependencies.py")
+    restricted=$(echo $requirements | ${SYSTEM_PYTHON} "$contrib/deterministic-build/find_restricted_dependencies.py")
     if [ ! -z "$restricted" ]; then
         python -m pip install $restricted
         requirements=$(pip freeze --all)
@@ -66,7 +66,7 @@ for suffix in '' '-hw' '-binaries' '-binaries-mac' '-build-wine' '-build-mac' '-
     fi
 
     echo -e "\r  Hashing requirements for $reqfile..."
-    python -m hashin $HASHIN_FLAGS -r "$contrib/deterministic-build/${reqfile}" $requirements
+    ${SYSTEM_PYTHON} -m hashin $HASHIN_FLAGS -r "$contrib/deterministic-build/${reqfile}" $requirements
 
     echo "OK."
 done
